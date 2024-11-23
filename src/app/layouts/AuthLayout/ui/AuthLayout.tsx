fff3b7ede31e0e49a696dee3646c60ab1a062715
Egor <egor032324@mail.ru>
2024-11-23 23:40:39 +0300

    fix: update max-width on auth layout

--- src/app/layouts/AuthLayout/ui/AuthLayout.tsx
+++ src/app/layouts/AuthLayout/ui/AuthLayout.tsx
@@ -6,7 +6,7 @@ import { Suspense } from "react";
 export const AuthLayout = () => {
   return (
     <Container
-      maxWidth="xs"
+      maxWidth="md"
       sx={{
         display: "flex",
         flexDirection: "column",
